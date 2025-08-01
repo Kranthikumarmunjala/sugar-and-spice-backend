50efb096cfcb06e8ad24639341f637867b91a089
Your Name <youremail@example.com>
2025-08-01 16:13:00 +0530

    Refactor database settings for production

--- sugarandspice/settings.py
+++ sugarandspice/settings.py
@@ -125,13 +125,33 @@ SITE_URL = 'http://127.0.0.1:8000'
 
 
 
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+#         conn_max_age=600,
+#         ssl_require=True  # <-- ADD THIS LINE
+#     )
+# }
+
+
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
-        conn_max_age=600,
-        ssl_require=True  # <-- ADD THIS LINE
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# This line checks if we are on Render by looking for the DATABASE_URL.
+# If it exists, it overrides the default SQLite database with the Render PostgreSQL database.
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config(
+        conn_max_age=600,
+        ssl_require=True
+    )
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
